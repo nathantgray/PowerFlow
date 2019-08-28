fixed data so that Ybus and Jacobian match

PF_Jacobian.py
@@ -79,6 +79,6 @@ def PF_Jacobian(v, d, y, pq):
 	jtop = np.concatenate((j11, j12), axis=1)
 	jbottom = np.concatenate((j21, j22), axis=1)
 	jacobian = np.concatenate((jtop, jbottom), axis=0)
-	return jacobian, j11, j22, j12, j22
+	return jacobian, j11, j21, j12, j22
 
 

PF_NewtonRaphson.py
@@ -1,54 +1,53 @@
 import numpy as np
-def PF_NewtonRaphson(d, V, Y, PQ, PV, Psched, Qsched, Qd, Qlim, prec, varargin):
-
-# Uses Newton-Raphson method to solve the power-flow of a power system.
-# Also capable of Q limiting.
-# Written by Nathan Gray
-# Arguments:
-# d: list of voltage phase angles in system
-# V: list of voltage magnitudes in system
-# Y: Ybus matrix for system
-# PQ: list of PQ busses
-# PV: list of PV busses
-# Psched, Qsched: list of real, reactive power injections
-# Qd: Reactive power demand (Qd and Qlim are not used in this version)
-# Qlim: array of Q limits- 1st column is maximums 2nd is minimums
-# prec: program finishes when all mismatches < 10^-abs(prec)
-# varargin: optional argument to specify maximum iterations
-if ~isempty(varargin):
-    # 'varargin' is an optional argument specifying the
-    # maximum number of iterations.
-    iterations = varargin{1}
-else:
-    # Default # iterations is 20.
-    iterations = 10
-
-N = np.shape(Y)[0]
-#Ng = length(PV);
-
-gMaxQ = []
-gMinQ = []
-## Newton Raphson
-for it in range(iterations):
-    # Calculate Jacobian
-    J = PF_Jacobian_fast(PQ,Y,V,d)
-    # Calculate Mismataches
-    mis = mismatch(d,V,Y,PQ,PV, Psched, Qsched)
-    # Calculate update values
-    dx =J^-1*mis
-    # Update angles: d_(n+1) = d_n + dd
-    d(sort([PV,PQ])) = d(sort([PV,PQ])) + dx(1:N-1)
-    # Update Voltages: V_(n+1) = V_n(1+dV/V_n)
-    V(PQ) = V(PQ).*(1+dx(N:N+length(PQ)-1))
-    # Check error
-    mis = mismatch(d,V,Y,PQ,PV, Psched, Qsched)
-    if max(mis) < 10^-abs(prec)
-        print("Newton Raphson completed in %i iterations.\n",it)
-        break
+from mismatch import mismatch
+from read_testcase import readCase
+from makeYbus import makeYbus
+from PF_Jacobian import PF_Jacobian
+from numpy.linalg import inv
 
 
-    if it>iterations-1
-        print("Max iterations reached, %i.\n",it)
+def PF_NewtonRaphson(v, d, y, pq, pv, psched, qsched, prec=2, maxit=5):
 
+	# Uses Newton-Raphson method to solve the power-flow of a power system.
+	# Also capable of Q limiting.
+	# Written by Nathan Gray
+	# Arguments:
+	# d: list of voltage phase angles in system
+	# V: list of voltage magnitudes in system
+	# Y: Ybus matrix for system
+	# PQ: list of PQ busses
+	# PV: list of PV busses
+	# Psched, Qsched: list of real, reactive power injections
+	# Qd: Reactive power demand (Qd and Qlim are not used in this version)
+	# Qlim: array of Q limits- 1st column is maximums 2nd is minimums
+	# prec: program finishes when all mismatches < 10^-abs(prec)
+	maxit = 4
 
+	n = np.shape(y)[0]
+	#Ng = length(PV);
+
+	gMaxQ = []
+	gMinQ = []
+	## Newton Raphson
+	for it in range(maxit):
+		# Calculate Jacobian
+		j, j11, j21, j12, j22 = PF_Jacobian(v, d, y, pq)
+		# Calculate Mismatches
+		mis, pcalc, qcalc = mismatch(v, d, y, pq, pv, psched, qsched)
+		# Calculate update values
+		dx =np.dot(inv(j), mis)
+		# Update angles: d_(n+1) = d_n + dd
+		d[np.sort(np.concatenate((pv, pq)))] = d[np.sort(np.concatenate((pv, pq)))] + dx[:n - 1]
+		# Update Voltages: V_(n+1) = V_n(1+dV/V_n)
+		v[pq] = v[pq]*(1+dx[n-1:n+pq.size-1])
+		# Check error
+		mis, pcalc, qcalc = mismatch(v, d, y, pq, pv, psched, qsched)
+		if max(mis) < 10**-abs(prec):
+			print("Newton Raphson completed in ", it, " iterations.")
+			break
+
+		if it >= maxit:
+			print("Max iterations reached, ", it, ".")
+
+	return v, d, it
 

makeYbus.py
@@ -1,7 +1,5 @@
 from read_testcase import readCase
-from reorder import reorder
 import numpy as np
-from math import cos, sin, pi, radians
 def makeYbus(filename):
 	busNumber = 0
 	busArea = 2
@@ -47,8 +45,8 @@ def makeYbus(filename):
 	# Create the four entries of a Y-Bus matrix for each line.
 	Ytt = branchY + 1j*branchB/2
 	Yff = Ytt/(abs(tap)**2)
-	Yft = -Ytt/np.conj(tap)
-	Ytf = -Ytt/tap
+	Yft = -branchY/np.conj(tap)
+	Ytf = -branchY/tap
 	# Shunt admittances for each bus.
 	Ysh =  busData[:, busShuntG]+ 1j*busData[:, busShuntB]
 	for i in range(nl):
@@ -61,7 +59,4 @@ def makeYbus(filename):
 	for i in range(n):
 		Ybus[i,i] += Ysh[i]
 
-	#print(Ybus)
-	#print('\n')
-	#print(branchData)
 	return Ybus

mismatch.py
@@ -20,5 +20,5 @@ def mismatch(v, d, y, pq, pv, psched, qsched):
 	qcalc = s[pq].imag
 	dp = psched-pcalc
 	dq = qsched-qcalc
-	mis = np.concatenate((dp, dq))
+	mis = np.transpose(np.concatenate((dp, dq)))
 	return mis, pcalc, qcalc