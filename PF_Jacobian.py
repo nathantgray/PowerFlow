fixed jacobian, still doesn't converge

PF_Jacobian.py
@@ -6,10 +6,10 @@ def pf_jacobian(v, d, y, pq):
 	# "Power Systems Analysis" J. Grainger et al.
 	# Calculates the Jacobian Matrix for use in the Newton-Raphson Method.
 	# Arguments:
-	# PQ: List of PQ buses
-	# Y: Ybus matrix
-	# V: Voltage magnitudes
+	# v: Voltage magnitudes
 	# d: Voltage phase angles
+	# y: Ybus matrix
+	# pq: List of PQ buses
 	n = y.shape[0]
 	s = (v * np.exp(1j * d)) * np.conj(np.dot(y, v * np.exp(1j * d)))
 	p = s.real
@@ -22,7 +22,7 @@ def pf_jacobian(v, d, y, pq):
 		j = col[a]
 		if i != 0 and j != 0:
 			if i == j:
-				j11[i-1, j-1] = q[i] - v[i]**2*y[i, i].imag
+				j11[i-1, j-1] = - q[i] - v[i]**2*y[i, i].imag
 			else:
 				j11[i-1, j-1] = -abs(v[i]*v[j]*y[i, j])*np.sin(np.angle(y[i, j]) + d[j] - d[i])
 
@@ -34,7 +34,7 @@ def pf_jacobian(v, d, y, pq):
 	for a in range(index.shape[0]):
 		i = index[a, 0]
 		j = index[a, 1]
-		k = np.where(pq == i)
+		k: int = np.where(pq == i)  # map bus index to jacobian index
 		if i == j:
 			j21[k, j-1] = p[i] - abs(v[i])**2*y[i, j].real
 		else:
@@ -48,7 +48,7 @@ def pf_jacobian(v, d, y, pq):
 	for a in range(index.shape[0]):
 		i = index[a, 0]
 		j = index[a, 1]
-		l: int = np.where(pq == j)
+		l: int = np.where(pq == j)  # map bus index to jacobian index
 		if i == j:
 			j12[i - 1, l] = p[i] + abs(v[i]**2*y[i, j].real)
 		else:
@@ -62,8 +62,8 @@ def pf_jacobian(v, d, y, pq):
 	for a in range(index.shape[0]):
 		i = index[a, 0]
 		j = index[a, 1]
-		k: int = np.where(pq == i)
-		l: int = np.where(pq == j)
+		k: int = np.where(pq == i)  # map bus index to jacobian index
+		l: int = np.where(pq == j)  # map bus index to jacobian index
 		if i == j:
 			j22[k, l] = -j11[i-1, j-1] - 2*abs(v[i])**2*y[i, j].imag
 		else:

PF_NewtonRaphson.py
@@ -5,20 +5,18 @@ from numpy.linalg import inv
 
 
 def pf_newtonraphson(v, d, y, pq, pv, psched, qsched, prec=2, maxit=4):
-
 	# Uses Newton-Raphson method to solve the power-flow of a power system.
-	# Also capable of Q limiting.
 	# Written by Nathan Gray
 	# Arguments:
+	# v: list of voltage magnitudes in system
 	# d: list of voltage phase angles in system
-	# V: list of voltage magnitudes in system
-	# Y: Ybus matrix for system
-	# PQ: list of PQ buses
-	# PV: list of PV buses
-	# Psched, Qsched: list of real, reactive power injections
+	# y: Ybus matrix for system
+	# pq: list of PQ buses
+	# pv: list of PV buses
+	# psched, qsched: list of real, reactive power injections
 	# prec: program finishes when all mismatches < 10^-abs(prec)
-	pvpq = np.sort(np.concatenate((pv, pq)))
 
+	pvpq = np.sort(np.concatenate((pv, pq)))
 	n = np.shape(y)[0]
 	# Newton Raphson
 	it = 0

makeYbus.py
@@ -1,27 +1,10 @@
 from read_testcase import readCase
 import numpy as np
+
+
 def makeybus(filename):
-	busNumber = 0
-	busArea = 2
-	busZone = 3
-	busType = 4
-	busFinalVoltage = 5
-	busFinalAngle = 6
-	busLoadMW = 7
-	busLoadMVAR = 8
-	busGenMW = 9
-	busGenMVAR = 10
-	busBaseKV = 11
-	busDesiredVolts = 12
-	busMaxMVAR = 13
-	busMinMVAR = 14
 	busShuntG = 15
 	busShuntB = 16
-	busRemoteControlledBusNumber = 17
-
-	branchTapBus = 0
-	branchZBus = 1
-
 
 	branchR = 6
 	branchX = 7
@@ -30,15 +13,14 @@ def makeybus(filename):
 	branchPhaseShift = 15
 
 	busData, branchData = readCase(filename)
-	#busData, branchData = reorder(busData, branchData)
 
 	nl = np.shape(branchData)[0]  # number of lines
 	n = np.shape(busData)[0]  # number of buses
-	Ybus = np.zeros((n, n)) + np.zeros((n, n))*1j # initialize Y Bus Matrix
+	Ybus = np.zeros((n, n)) + np.zeros((n, n))*1j  # initialize Y Bus Matrix
 	branchZ = branchData[:, branchR] + branchData[:, branchX]*1j
 	branchY = branchZ**-1
 	branchB = branchData[:, branchB]
-	ratio = np.where(branchData[:, branchTurnsRatio]==0.0, 1, branchData[:, branchTurnsRatio])
+	ratio = np.where(branchData[:, branchTurnsRatio] == 0.0, 1, branchData[:, branchTurnsRatio])
 	shift = np.radians(branchData[:, branchPhaseShift])
 	tap = ratio*np.cos(shift) + 1j*ratio*np.sin(shift)
 
@@ -48,15 +30,15 @@ def makeybus(filename):
 	Yft = -branchY/np.conj(tap)
 	Ytf = -branchY/tap
 	# Shunt admittances for each bus.
-	Ysh =  busData[:, busShuntG]+ 1j*busData[:, busShuntB]
+	Ysh = busData[:, busShuntG] + 1j*busData[:, busShuntB]
 	for i in range(nl):
 		frombus = int(branchData[i, 0])-1
-		tobus = int(branchData[i,1])-1
+		tobus = int(branchData[i, 1])-1
 		Ybus[frombus, tobus] += Yft[i]
 		Ybus[tobus, frombus] += Ytf[i]
 		Ybus[frombus, frombus] += Yff[i]
 		Ybus[tobus, tobus] += Ytt[i]
 	for i in range(n):
-		Ybus[i,i] += Ysh[i]
+		Ybus[i, i] += Ysh[i]
 
 	return Ybus

mismatch.py
@@ -7,16 +7,19 @@ def mismatch(v, d, y, pq, pv, psched, qsched):
 	# injections in a system vs. the scheduled injections.
 	# power system network.
 	# Arguments:
+	# v: list of voltage magnitudes in system
 	# d: list of voltage phase angles in system
-	# V: list of voltage magnitudes in system
-	# Y: Ybus matrix for system
-	# PQ: list of PQ buses
-	# PV: list of PV buses
-	# Psched, Qsched: list of real, reactive power injections
+	# y: Ybus matrix for system
+	# pq: list of PQ buses
+	# pv: list of PV buses
+	# psched, qsched: list of real, reactive power injections
+	#
 	# S = V*conj(I) and I = Y*V => S = V*conj(Y*V)
 	# S = P + jQ
+
+	pvpq = np.sort(np.concatenate((pv, pq)))
 	s = (v*np.exp(1j*d))*np.conj(y.dot(v*np.exp(1j*d)))
-	pcalc = s[np.concatenate((pv, pq))].real
+	pcalc = s[pvpq].real
 	qcalc = s[pq].imag
 	dp = psched-pcalc
 	dq = qsched-qcalc